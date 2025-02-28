4876ecaf231d6c39688abf6376e4118491f83d58
Antonio Mendoza Pérez <antonio.perez@temporal.io>
2025-02-28 07:45:21 +0100

    wip

--- go-sdk-metrics/README.md
+++ go-sdk-metrics/README.md
@@ -3,13 +3,29 @@
 See [start-env.md](../start-env.md)
 
 - Install dependencies
+
+
 ```bash
 go mod tidy
 ```
 
+
+```bash
+cd opentelemetry-collector-contrib
+docker-compose down -v
+docker-compose up 
+```
+
+http://localhost:9091
+
+
+
 - Start the client
 
 ```bash
+
+## command with variable to enable telemetry 
+export ENABLE_TELEMETRY=true
 go run starter/main.go
 ```
 
@@ -17,5 +33,12 @@ go run starter/main.go
 - Start the worker
 
 ```bash
+## command with variable to enable telemetry 
+export ENABLE_TELEMETRY=true
 go run worker/main.go
 ```
+
+
+
+
+
--- go-sdk-metrics/worker/main.go
+++ go-sdk-metrics/worker/main.go
@@ -1,39 +1,53 @@
 package main
 
 import (
+	"context"
 	"github.com/uber-go/tally/v4/prometheus"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/sdk/metric"
 	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/contrib/opentelemetry"
 	sdktally "go.temporal.io/sdk/contrib/tally"
 	"go.temporal.io/sdk/worker"
 	"log"
+	"os"
 	"sample_5888"
+	"time"
 )
 
 func main() {
 
-	//	ctx := context.Background()
-	//exp, err := otlpmetrichttp.New(ctx, otlpmetrichttp.WithEndpointURL("http://localhost:4318"))
-	//if err != nil {
-	//	panic(err)
-	//}
-	//meterProvider := metric.NewMeterProvider(metric.WithReader(
-	//	metric.NewPeriodicReader(exp, metric.WithInterval(1*time.Second)),
-	//))
-	//handler := opentelemetry.NewMetricsHandler(
-	//	opentelemetry.MetricsHandlerOptions{
-	//		Meter: meterProvider.Meter("temporal-sdk-go"),
-	//	},
-	//)
+	var metricsHandler client.MetricsHandler = nil
 
-	tallyHandler := sdktally.NewMetricsHandler(sample_5888.NewPrometheusScope(prometheus.Configuration{
-		ListenAddress: "0.0.0.0:9095",
-		TimerType:     "histogram",
-	}))
+	if os.Getenv("ENABLE_TELEMETRY") == "true" {
+
+		ctx := context.Background()
+		exp, err := otlpmetrichttp.New(ctx, otlpmetrichttp.WithEndpointURL("http://localhost:4318"))
+		if err != nil {
+			panic(err)
+		}
+		meterProvider := metric.NewMeterProvider(metric.WithReader(
+			metric.NewPeriodicReader(exp, metric.WithInterval(1*time.Second)),
+		))
+
+		metricsHandler = opentelemetry.NewMetricsHandler(
+			opentelemetry.MetricsHandlerOptions{
+				Meter: meterProvider.Meter("temporal-sdk-go"),
+			},
+		)
+
+	} else {
+
+		metricsHandler = sdktally.NewMetricsHandler(sample_5888.NewPrometheusScope(prometheus.Configuration{
+			ListenAddress: "0.0.0.0:9095",
+			TimerType:     "histogram",
+		}))
+
+	}
 
 	c, err := client.Dial(client.Options{
-		MetricsHandler: tallyHandler,
+		MetricsHandler: metricsHandler,
 	})
-
 	defer c.Close()
 
 	w := worker.New(c, "metrics", worker.Options{})
